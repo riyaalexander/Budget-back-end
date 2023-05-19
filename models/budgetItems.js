15d738c6d013713114cac123b774ba6788b98bd3
riyaalexander <riyaalexander@Riyas-MacBook-Air.local>
2023-05-18 21:59:40 -0400

    fixes

--- models/budgetItems.js
+++ models/budgetItems.js
@@ -1,13 +1,12 @@
-const { v4: uuid4 } = require('uuid')
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = [
-    {
-    id: uuid4(),
+  {
+    id: uuidv4(),
     itemName: 'dog',
     amount: 10000,
     date: '1/2/22',
     from: 'boss',
     category: 'dog',
-        
-    }
-]
+  }
+];
