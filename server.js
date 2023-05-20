48a0ee3d8f06178bf34f957b070a413c032174ca
riyaalexander <riyaalexander@Riyas-MacBook-Air.local>
2023-05-20 14:33:31 -0400

    styling

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = 5555;
 
 app.use(bodyParser.json());
 app.use(cors());
